e05a7337fcec8a1816a97c27f3c70a6d53659cdd
Nathan <nathan@lb.dev>
2024-11-22 01:07:20 -0300

    Music on menu added

--- Mars_base/Assets/Scripts/Menu/MenuController.cs
+++ Mars_base/Assets/Scripts/Menu/MenuController.cs
@@ -5,15 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-
+    [Header("Screens")]
     public GameObject firstScreen;
     public GameObject briefingScreen;
+
+    [Header("Audio Controllers")]
+    public AudioSource musicaMenu;
+    public GameObject musicaOn;
+    public GameObject musicaOff;
 
     // Start is called before the first frame update
     void Start()
     {
         firstScreen.SetActive(true);
         briefingScreen.SetActive(false);
+        musicaOff.SetActive(false);
+        musicaOn.SetActive(true);
+
     }
 
     // Update is called once per frame
@@ -31,6 +39,24 @@ public class MenuController : MonoBehaviour
     public void initGame()
     {
         SceneManager.LoadScene("Test1_mars");
+    }
+
+    public void MusicOnOff()
+    {
+        if(musicaMenu.volume == 1)
+        {
+            musicaMenu.volume = 0;
+            musicaOff.SetActive(true);
+            musicaOn.SetActive(false);
+            return;
+        }
+        if (musicaMenu.volume == 0)
+        {
+            musicaMenu.volume = 1;
+            musicaOff.SetActive(false);
+            musicaOn.SetActive(true);
+            return;
+        }
     }
 
 }
